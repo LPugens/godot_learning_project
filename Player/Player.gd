24635716a4946ac81cf461e063c2298f0272d52e
Lucas Pugens Fernandes <lpugens@amazon.com>
2021-10-25 00:26:27 -0300

    P21 done

--- Player/Player.gd
+++ Player/Player.gd
@@ -67,7 +67,6 @@ func roll_state(delta):
 	velocity = roll_vector * ROLL_SPEED
 	animationState.travel("Roll")
 	move()
-	
 
 func attack_state(delta):
 	velocity = Vector2.ZERO
--- World/Grass.gd
+++ World/Grass.gd
@@ -4,7 +4,6 @@ const GrassEffect = preload("res://Effects/GrassEffect.tscn")
 
 func create_grass_effect():
 	var grassEffect = GrassEffect.instance()
-	var world = get_tree().current_scene
 	get_parent().add_child(grassEffect)
 	grassEffect.global_position = global_position
 
